dd41e896235054778e274b25060713a02a8dd537
Nguyen Quang Huy <qhnguyen81100@gmail.com>
2021-05-02 18:31:48 +0700

    Del cmt

--- app/src/main/java/com/example/doctruyenapp/EditProfileActivity.java
+++ app/src/main/java/com/example/doctruyenapp/EditProfileActivity.java
@@ -10,5 +10,5 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
-    } ////////////////////////////////
+    }
 }
