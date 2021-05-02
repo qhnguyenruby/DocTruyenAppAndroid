42d5deb046a14474faf447a1f75790e02fd473eb
Nguyen Quang Huy <qhnguyen81100@gmail.com>
2021-05-02 15:56:02 +0700

    add edit profile activity

--- app/src/main/java/com/example/doctruyenapp/ProfileFragment.java
+++ app/src/main/java/com/example/doctruyenapp/ProfileFragment.java
@@ -1,17 +1,22 @@
 package com.example.doctruyenapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class ProfileFragment extends Fragment {
 
 
+    Button btnEdit;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -24,5 +29,19 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnEdit = view.findViewById(R.id.btn_edit);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
