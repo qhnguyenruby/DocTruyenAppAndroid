493ffdfefe5b348a306d6541c253c5f27ba9489f
Tungdeptrai <tranhuytung2810@gmail.com>
2021-05-16 14:29:28 +0700

    alo1234 t chịu

--- app/src/main/java/com/example/doctruyenapp/LoginActivity.java
+++ app/src/main/java/com/example/doctruyenapp/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.doctruyenapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
@@ -10,11 +12,19 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    ArrayList<String> userlist = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +39,39 @@ public class LoginActivity extends AppCompatActivity {
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
 
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference nguoidung1 = firebaseDatabase.getReference("nguoidung/1");
+        DatabaseReference nguoidung2 = firebaseDatabase.getReference("nguoidung/2");
+        DatabaseReference nguoidung3 = firebaseDatabase.getReference("nguoidung/3");
+        DatabaseReference nguoidung4 = firebaseDatabase.getReference("nguoidung/4");
 
+
+        nguoidung1.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/doctruyenapp/UserProfileActivity.java
+++ app/src/main/java/com/example/doctruyenapp/UserProfileActivity.java
@@ -1,22 +1,37 @@
 package com.example.doctruyenapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    String s;
+    ArrayList<String> arrayuser = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        s = "1"; //Lấy s từ đoạn login
 
         tabLayout = findViewById(R.id.tab_layout1);
         viewPager = findViewById(R.id.view_pager1);
@@ -25,5 +40,36 @@ public class UserProfileActivity extends AppCompatActivity {
         UserProfileAdapter userProfileAdapter = new UserProfileAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         viewPager.setAdapter(userProfileAdapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference User = database.getReference("NguoiDung/"+s);
+
+        User.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = snapshot.getValue(String.class);
+                arrayuser.add(value);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
